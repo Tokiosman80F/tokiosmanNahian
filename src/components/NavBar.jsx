dc340e363c0e3fbc167b48d795da4f23e0b715e5
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2023-06-08 21:36:28 +0600

    nav bar for mbl vesion is created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,10 +29,23 @@ const NavBar = () => {
         onClick={() => setNav(!nav)}
         className="cursor-pointer text-3xl text-gray-500"
       >
-        {nav ? <FaBars className=" " /> : <FaTimes />}
+        {nav ? <FaBars /> : <FaTimes />}
       </div>
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
+        {links.map(({ link, id }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 cursor-pointer uppercase py-6 text-4xl"
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
 export default NavBar;
+// className="px-4 cursor-pointer uppercase py-6 text-4xl"
