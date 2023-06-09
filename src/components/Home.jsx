c3d027511502f986ff151d9786b2200b29b73510
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2023-06-09 07:08:59 +0600

    fix the nav bar issue for mbl screen and completed the hero section

---
+++ src/components/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FiDownload } from "react-icons/fi";
+import Lottie from "lottie-react";
+import avatar from "../assets/heroImh.json";
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black
+    to-gray-800"
+    >
+      {/* main */}
+      <div className="max-w-screen-lg mx-auto flex flex-col-reverse items-center justify-center h-full px-4 md:flex-row ">
+        {/* right side content */}
+        <div className=" flex flex-col justify-center h-full">
+          <h1
+            className="text-4xl sm:text-7xl font-bold text-white
+          "
+          >
+            Hello I'm a Full stack developer
+          </h1>
+          <p className="text-gray-500 py-4 max-w-md">
+            I have 8 Year of experience building and designing sde.Current I
+            have no work so i start building this portfilio so that i can get a
+            job.Now give me job
+          </p>
+
+          <div>
+            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 gap-2">
+              Portfolio
+              <span className="animate-pulse">
+                <FiDownload />
+              </span>
+            </button>
+          </div>
+        </div>
+        {/* left side image */}
+        <div>
+          <div>
+            <Lottie animationData={avatar}></Lottie>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaBars, FaBeer, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -27,22 +27,24 @@ const NavBar = () => {
       </ul>
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer text-3xl text-gray-500"
+        className="cursor-pointer text-3xl text-gray-500 md:hidden"
       >
         {nav ? <FaBars /> : <FaTimes />}
       </div>
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
-        {links.map(({ link, id }) => {
-          return (
-            <li
-              key={id}
-              className="px-4 cursor-pointer uppercase py-6 text-4xl"
-            >
-              {link}
-            </li>
-          );
-        })}
-      </ul>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-20 left-0 w-full h-full bg-gradient-to-b from-black to-gray-800 text-gray-400">
+          {links.map(({ link, id }) => {
+            return (
+              <li
+                key={id}
+                className="px-4 cursor-pointer uppercase py-6 text-4xl hover:scale-125 transition duration-200"
+              >
+                {link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
